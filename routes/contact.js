5e3125048ba8b15502d59b1bb82f4e877eb7464a
mutant138 <udhayasurya138@gmail.com>
2023-10-06 19:54:50 +0530

    Node continuation

---
+++ routes/contact.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const path = require('path')
+
+const router = express.Router();
+const rootDir = require('../util/path')
+
+router.get('/contactus',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contact.html'))
+});
+
+
+module.exports = router
